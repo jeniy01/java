91273542d99375e92d76057884937719f74e85fa
jeniy01 <dallown@naver.com>
2023-03-15 18:08:42 +0900

    java

--- ch05/src/sec1/Example2.java
+++ ch05/src/sec1/Example2.java
@@ -12,6 +12,17 @@ public class Example2 {
 		String st_arr[]=new String[5];
 		System.out.println("io_arr="+io_arr);
 		System.out.println("st_arr="+st_arr);
+		
+		int[] arr={1,80,90,70}; //몇개의 원소가 특정되므로 주소 배정이 되고, 초기화가 됨
+		int arr2[]={2,90,100,80};
+		System.out.println("arr주소 : "+arr);
+		System.out.println("arr2주소 : "+arr2);
+		for(int i=0;i<arr.length;i++){
+			System.out.print(arr[i]+"\t");
+		}
+		for(int data : arr2){ //추가 향상된 for문
+			System.out.print(data+"\t");
+		}
 	}
 
 }
--- ch05/src/sec1/Example3.java
+++ ch05/src/sec1/Example3.java
@@ -2,14 +2,33 @@ package sec1;
 
 public class Example3 {
 	public static void main(String[] args) {
-		int[]arr1;
+		int[]arr1; //아직 주소 배정 안된 상태
 		int[]arr2;
 		int[]arr3;
-		
+		//데이터 개수와 실체가 파악되어 실제 주소 배정이 이루어짐 이때 배정된 주소를 포인터라 함
 		arr1=new int[]{1,90,70,80};
 		arr2=new int[]{1,90,70,80};
-		arr3=arr2;
-		System.out.println("arr1");
+		arr3=arr2; //얕은 복제 : 실제 데이터의 주소만 전달하므로 주소가 같다.
+		System.out.println("arr1 : "+arr1);
+		System.out.println("arr2 : "+arr2);
+		System.out.println("arr3 : "+arr3);
+		for(int data : arr1){
+			System.out.print(data+"\t");
+		}
+		System.out.println();
+		for(int data : arr2){
+			System.out.print(data+"\t");
+		}
+		System.out.println();
+		for(int data : arr3){
+			System.out.print(data+"\t");
+		}
+		System.out.println();
+		System.out.println("arr1==arr2 : "+(arr1==arr2));
+		System.out.println("arr2==arr3 : "+(arr2==arr3));
+		System.out.println();
+		System.out.println("arr1!=arr2 : "+(arr1!=arr2));
+		System.out.println("arr2!=arr3 : "+(arr2!=arr3));
 		}
 
 }
