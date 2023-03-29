2b5205a1506153f49b108490e744eec01fe37c75
jeniy01 <dallown@naver.com>
2023-03-29 16:23:45 +0900

    java

---
+++ ch10/src/pack2/Board.java
@@ -0,0 +1,45 @@
+package pack2;
+
+public class Board {
+	private String bno;
+	private String title;
+	private String content;
+	private String author;
+	private String resdate;
+	public String getBno() {
+		return bno;
+	}
+	public void setBno(String bno) {
+		this.bno = bno;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getResdate() {
+		return resdate;
+	}
+	public void setResdate(String resdate) {
+		this.resdate = resdate;
+	}
+	@Override
+	public String toString() {
+		return "Board [bno=" + bno + ", title=" + title + ", content="
+				+ content + ", author=" + author + ", resdate=" + resdate + "]";
+	}
+	
+}
