143485c43554d2bc449714c74161ddae3e172928
jeniy01 <dallown@naver.com>
2023-04-04 15:55:35 +0900

    java

--- ch15/src/sec1/ArrayListEx3.java
+++ ch15/src/sec1/ArrayListEx3.java
@@ -24,6 +24,45 @@ public class ArrayListEx3 {
 				st.setEng(Integer.parseInt(sc.nextLine()));
 				System.out.println("수학 : ");
 				st.setMat(Integer.parseInt(sc.nextLine()));
+				stArr.add(st);
+			} else if(job==2){
+				for(Student s : stArr){
+					System.out.println(s.toString());
+				}
+			} else if(job==3){
+				System.out.println("삭제할 번호(인덱스) 입력 : ");
+				int a = Integer.parseInt(sc.nextLine());
+				if(a >= stArr.size()){
+					System.out.println("해당 번호의 데이터가 존재하지 않습니다.");
+				} else {
+					stArr.remove(a);
+				}
+			} else if(job==4){
+				System.out.print("값을 변경할 번호(인덱스) 입력 : ");
+				int a = Integer.parseInt(sc.nextLine());
+				if(a >= stArr.size()){
+					System.out.println("해당 번호의 데이터가 존재하지 않습니다.");
+				} else {
+					stArr.remove(a);
+					Student st = new Student();
+					st.setNo(cnt);
+					System.out.print("이름 : ");
+					st.setName(sc.nextLine());
+					System.out.print("국어 : ");
+					st.setKor(Integer.parseInt(sc.nextLine()));
+					System.out.print("영어 : ");
+					st.setEng(Integer.parseInt(sc.nextLine()));
+					System.out.print("수학 : ");
+					st.setMat(Integer.parseInt(sc.nextLine()));
+					stArr.add(a, st);
+				}
+			} else if(job==5) {
+				System.out.println("학생데이터를 모두 비웁니다.");
+				cnt = 0;
+				stArr.clear();	//ArrayList 비우기
+			} else {
+				System.out.println("프로그램 종료");
+				break;
 			}
 		}
 	}
--- ch15/src/sec1/VectorEx1.java
+++ ch15/src/sec1/VectorEx1.java
@@ -1,7 +1,7 @@
 package sec1;
 
 import java.util.*;
-//Vector는 동기화되는 싱글쓰레드에서 사용, ArrayList는 멀티쓰레드, 싱글쓰레드 구분 없이 사용
+//Vector는 동기화되는 싱글 쓰레드에서 사용, ArrayList는 멀티 쓰레드, 싱글 쓰레드 구분없이 사용
 public class VectorEx1 {
 	public static void main(String[] args) {
 		List<Integer> v1 = new Vector<Integer>();
@@ -13,27 +13,37 @@ public class VectorEx1 {
 		for(int i=0;i<v1.size();i++){
 			System.out.println(v1.get(i));
 		}
-		System.out.println("-------------");
+		System.out.println("-----------------------------------");
 		for(Integer d : v1){
 			System.out.println(d);
 		}
-		System.out.println("*************");
-		v1.clear();	//비우기
+		System.out.println("**************************************");
+		v1.clear();	//비우기 
+		for(Integer d : v1){
+			System.out.println(d);
+		}
+		System.out.println();
+		Vector<String> v2 = new Vector<>();
+		v2.add("김기태");
+		v2.add("김세종");
+		v2.add("김우주");
+		v2.add("김지영");
+		for(String s:v2){
+			System.out.println(s);
+		}
 		
-		List<Student> v3 = new Vector<Student>();
+		List<Student> v3 = new Vector<>();
 		Student st = new Student();
 		st.setNo(1);
-		st.setName("이동혁");
+		st.setName("김기태");
 		st.setKor(100);
-		st.setEng(90);
-		st.setMat(80);
+		st.setEng(100);
+		st.setMat(100);
 		v3.add(st);
 		for(Student s : v3){
 			System.out.println(s.toString());
 		}
-		
 		//Iterator를 활용한 요소 분리와 while 반복문
-		List<String> v2 = new Vector<String>();
 		Iterator iter = v2.iterator();
 		while(iter.hasNext()){
 			System.out.println(iter.next());
